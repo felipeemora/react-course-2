ed49b2697a52d0d5dece9f6660e001f438b36ab7
Felipe Mora <juli.mora.5667@gmail.com>
2024-07-28 14:45:55 -0500

    add comments

--- src/02-components-pattern/components/ProductCard.tsx
+++ src/02-components-pattern/components/ProductCard.tsx
@@ -6,8 +6,10 @@ import { ProductContextProps, ProductCardProps } from '../interfaces/interfaces'
 export const ProductContext = createContext({} as ProductContextProps);
 const { Provider } = ProductContext;
 
+// Componente que recibe uno o varios hijos a renderizar y un producto a mostrar
+// se una un Product Context para envolver los hijos allí dentro y que tengan acceso
+// al producto que recibió este ProductCard
 export const ProductCard = ({ children, product }: ProductCardProps) => {
-
   const {counter, increaseBy} = useProduct();
 
   return (
--- src/02-components-pattern/components/index.ts
+++ src/02-components-pattern/components/index.ts
@@ -8,7 +8,8 @@ export { ProductButtons } from "./ProductButtons";
 export { ProductImage } from "./ProductImage";
 export { ProductTitle } from "./ProductTitle";
 
-
+// Esta asignación se hace para poder hacer ProductCard.Title donde se quiera usar
+// los hijos de product card
 export const ProductCard: ProductCardHOCProps = Object.assign(ProductCardHOC, {
   Title: ProductTitle,
   Image: ProductImage,
